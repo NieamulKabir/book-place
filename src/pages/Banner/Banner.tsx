d94cdb2924d192077e5a917645977a44b8b50b99
NieamulKabir <kabir000079@gmail.com>
2023-08-26 20:51:46 +0600

    add private route

--- src/pages/Banner/Banner.tsx
+++ src/pages/Banner/Banner.tsx
@@ -5,10 +5,10 @@ const Banner = () => {
         <div className="hero-content flex-col lg:flex-row">
           <img
             src="https://i.ibb.co/cwF3vmG/gone-girl.jpg"
-            className="max-w-sm rounded-lg shadow-2xl"
+            className="w-[50%] rounded-lg shadow-2xl"
           />
           <div className="text-white">
-            <h1 className="text-5xl font-bold">Box Office News!</h1>
+            <h1 className="text-5xl font-bold">Read Your Desire Book</h1>
             <p className="py-6">
               Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
               excepturi exercitationem quasi. In deleniti eaque aut repudiandae
---
+++ src/routes/PrivateRoute.tsx
@@ -0,0 +1,19 @@
+import { IProps } from "../types/globalTypes";
+import { useAppSelector } from "../redux/hook";
+import { Navigate, useLocation } from "react-router-dom";
+
+const PrivateRoute = ({ children }: IProps) => {
+  const { user, isLoading } = useAppSelector((state) => state.user);
+
+  const {pathname}= useLocation()
+
+  if(isLoading){
+    return <p>Loading ...</p>
+  }
+  if(!user?.email && !isLoading){
+    return <Navigate to='/login' state={{path:pathname}} />
+  }
+  return children;
+};
+
+export default PrivateRoute;
